f56b4065aa58241e59127ca6a182c9210c6db9f1
ArtiomSu <artiom.sumigora@mycit.ie>
2021-06-19 12:43:16 +0100

    initial commit

---
+++ Kensington_Expert_Setup.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# xinput list 
+# Default layout
+# "Button Middle"  	"Button Wheel Up"      "Button Horiz Wheel Left"    "Button Side"
+#       2                   4                         6						  8         "Button Extra"
+#                                                                                              9
+# "Button Left"     "Button Wheel Down"    "Button Horiz Wheel Right"   "Button Right"
+#       1					5                         7                       3
+
+
+# "Button Left" "Button Middle" "Button Right" "Button Wheel Up" "Button Wheel Down" "Button Horiz Wheel Left" "Button Horiz Wheel Right" "Button Side" "Button Extra"
+#       1              2             3              4                   5                  6                               7                   8             9
+#
+
+mouse_name="Kensington Expert Wireless TB Mouse"
+
+check=$(xinput | grep "$mouse_name")
+
+if [[ ! -z "$check" ]]; then
+	mouse_id=$(xinput | grep "$mouse_name" | sed 's/^.*id=\([0-9]*\)[ \t].*$/\1/')
+	# swap right and back button then swap middle and back button
+	xinput set-button-map $mouse_id 1 8 2 4 5 6 7 3 9
+	# enable better scrolling 
+	xinput set-prop $mouse_id "libinput Natural Scrolling Enabled" 1
+	# disable acceliration for the ball
+	xinput set-prop $mouse_id "libinput Accel Profile Enabled" 0, 1
+
+	# allow scrolling by holding middle mouse button and using the ball to scroll ( really smooth and fast ). 
+	# Note this uses the original middle click button, so like I have mapped my middle click to bottom right but I still need to use the top left button
+	xinput set-prop $mouse_id "libinput Scroll Method Enabled" 0, 0, 1
+fi
+
+# read more here https://askubuntu.com/questions/492744/how-do-i-automatically-remap-buttons-on-my-mouse-at-startup
+
+# Default properties, so if you change yours around alot you can refer here for default config.
+# xinput list-props
+# Device 'Kensington Expert Wireless TB Mouse':
+#	Device Enabled (153):	1
+#	Coordinate Transformation Matrix (155):	1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000
+#	libinput Natural Scrolling Enabled (289):	0
+#	libinput Natural Scrolling Enabled Default (290):	0
+#	libinput Scroll Methods Available (291):	0, 0, 1
+#	libinput Scroll Method Enabled (292):	0, 0, 0
+#	libinput Scroll Method Enabled Default (293):	0, 0, 0
+#	libinput Button Scrolling Button (294):	2
+#	libinput Button Scrolling Button Default (295):	2
+#	libinput Button Scrolling Button Lock Enabled (296):	0
+#	libinput Button Scrolling Button Lock Enabled Default (297):	0
+#	libinput Middle Emulation Enabled (298):	0
+#	libinput Middle Emulation Enabled Default (299):	0
+#	libinput Accel Speed (300):	0.000000
+#	libinput Accel Speed Default (301):	0.000000
+#	libinput Accel Profiles Available (302):	1, 1
+#	libinput Accel Profile Enabled (303):	1, 0
+#	libinput Accel Profile Enabled Default (304):	1, 0
+#	libinput Left Handed Enabled (305):	0
+#	libinput Left Handed Enabled Default (306):	0
+#	libinput Send Events Modes Available (274):	1, 0
+#	libinput Send Events Mode Enabled (275):	0, 0
+#	libinput Send Events Mode Enabled Default (276):	0, 0
+#	Device Node (277):	"/dev/input/event9"
+#	Device Product ID (278):	1149, 32792
+#	libinput Drag Lock Buttons (307):	<no items>
+#	libinput Horizontal Scroll Enabled (308):	1
---
+++ README.md
@@ -0,0 +1,35 @@
+# kensington-expert-trackball-linux-config
+
+This is a configuration script for the Kensington Expert trackball that remaps some of the buttons and edits some props.
+
+This scripts dependancies are `xinput` `grep` and `sed` which should be installed on all distros by default.
+
+There are comments in the script which should help you setup your own config.
+
+# Layout
+
+If you run the script as is without changing anything this will be your layout:
+```
+______________   _________  ________________
+| back       |   |       |  | right click  |
+--------------   |       |  ----------------
+______________   |       |  ________________
+| left click |   |       |  | middle click |
+--------------   ---------  ----------------
+
+```
+1. Natural scrolling will be enabled. ( scrolling is smoother with this on )
+2. Ball acceliration will be disabled.
+3. Middle click + ball roll can also be used to scroll. ( very smooth and fast scrolling but a bit awkward with the top left button )
+
+# Usage
+You can run the script simpy by calling the script `./Kensington_Expert_Setup.sh` there is no need for root.
+
+Or a better way is to put it in your desktop environment or window manager config, so that it runs when you login, the script also checks if the trackball is present before running any commands so its safe to use it like this.
+
+I use i3 window manager so I put this in my i3 config `exec_always --no-startup-id ~/scrips/Kensington_Expert_Setup.sh`
+
+# Todo
+I want to change the scroll speed without using any other external programs but this trackball is missing a property in xinput props to be able to do so.
+
+The kensington software on windows allows you to press two buttons at once for a different input which is pretty cool, so I'm looking for a way to achieve this on linux too. It doesn't seem possible with xinput to my knowledge. 
